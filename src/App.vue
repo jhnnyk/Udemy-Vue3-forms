3854fc6ca5736ce8f778b501b746ff23cea51e5c
John Knotts <jhnnyk@gmail.com>
2022-06-30 15:56:13 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
