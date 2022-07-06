faa9a2ae9d167bc336ecff413af6dfefae041085
John Knotts <jhnnyk@gmail.com>
2022-07-06 09:34:43 -0600

    keyboard events

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,6 +12,12 @@
       <option value="designer">Web Designer</option>
     </select>
 
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div class="pill" v-for="skill in skills" :key="skill">
+      {{ skill }}
+    </div>
+
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
@@ -47,7 +53,20 @@ export default {
       role: '',
       terms: false,
       names: [],
+      tempSkill: '',
+      skills: [],
     };
+  },
+
+  methods: {
+    addSkill(e) {
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = '';
+      }
+    },
   },
 };
 </script>
